bdca0f05e90edacefc10f2a6979cc216a4d8df81
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-17 13:07:40 -0400

    WE GOT THE TEST WORKING BITCHES. Alright we in business. Game time

--- app/entry.js
+++ app/entry.js
@@ -11,30 +11,30 @@ const ngTouch = require('angular-touch');
 const ngAnimate = require('angular-animate');
 const uiBootstrap = require('angular-ui-bootstrap');
 
-const crytocurrency = angular.module('test', [ngTouch, ngAnimate, uiRouter, uiBootstrap]);
+const cryptocurrency = angular.module('cryptocurrency', [ngTouch, ngAnimate, uiRouter, uiBootstrap]);
 
 let context = require.context('./config/', true, /\.js$/);
 context.keys().forEach( key => {
-  test.config(context(key));
+  cryptocurrency.config(context(key));
 });
 
 context = require.context('./view/', true, /\.js$/);
 context.keys().forEach( key => {
   let name = pascalcase(path.basename(key, '.js'));
   let module = context(key);
-  test.controller(name, module);
+  cryptocurrency.controller(name, module);
 });
 
 context = require.context('./service/', true, /\.js$/);
 context.keys().forEach( key => {
   let name = camelcase(path.basename(key, '.js'));
   let module = context(key);
-  test.service(name, module);
+  cryptocurrency.service(name, module);
 });
 
 context = require.context('./component/', true, /\.js$/);
 context.keys().forEach( key => {
   let name = camelcase(path.basename(key, '.js'));
   let module = context(key);
-  test.component(name, module);
+  cryptocurrency.component(name, module);
 });
