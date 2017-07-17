748a786dad5446d52cb620e11079add4f876a701
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-17 12:59:11 -0400

    additions of the basics of the app file have been completed

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT;
 app.use(express.static(`${__dirname}/build`));
 
 app.listen(PORT, () => {
-  console.log('crytocurrency initialized');
+  console.log('cryptocurrency initialized');
 });
